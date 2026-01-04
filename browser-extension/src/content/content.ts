56c8649907f165ce268d688dd2054f9ca74a8338
Samiran Pal <palsamiranpal2004@gmail.com>
2026-01-04 13:15:17 +0530

    feat: update save button styles and replace plus icon with SVG for better scalability

--- browser-extension/src/content/content.ts
+++ browser-extension/src/content/content.ts
@@ -3133,7 +3133,7 @@ function handleTextSelection() {
   saveButton.className = "linkedin-helper-save-btn";
   saveButton.innerHTML = `
     <button class="lh-save-selection-btn" title="Save as Quick Response">
-      <span class="lh-plus-icon">+</span>
+      <span class="lh-plus-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M2 18H12V20H2V18ZM2 11H22V13H2V11ZM2 4H22V6H2V4ZM18 18V15H20V18H23V20H20V23H18V20H15V18H18Z"></path></svg></span>
     </button>
   `;
 
